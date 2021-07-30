399ebaa0827933162e95411d3e9197b6c5c8493e
pavanyalala <pavanyalala4508@gmail.com>
2021-07-30 08:30:20 +0530

    [pavan]:Json Inventory Data Management

---
+++ Oops/Program.cs
@@ -0,0 +1,15 @@
+﻿using Oops.InventoryManagement;
+using System;
+
+namespace Oops
+{
+    class Program
+    {
+        const string INVENTORY_JSON =@"C:\Users\DELL\source\repos\Oops\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            Inventorymain inventoryMain = new Inventorymain();
+            inventoryMain.DisplayData(INVENTORY_JSON);
+        }
+    }
+}
